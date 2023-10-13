3653484a54ca83992020ca53afa6e7ea5737c618
ouozturk <ozan.ugur.ozturk@student.forsbergsskola.se>
2023-10-13 16:18:00 +0200

    Base app is working with:
    -5 days weather forecast
    -Some CSS acc. to my HTML implementation
    Requirements for the next steps:
    -Weather Icon changer/selector function
    -Search-bar
    -Default cities to be checked
    -Button to switch to default cities one-by-one

--- script.js
+++ script.js
@@ -1,67 +1,109 @@
-//Variables to store API key and API url
-const API_KEY = 'b99f308a84c42a7a2c701a26eba18058'
+// Function to format a timestamp to a readable time format
+function formatTime(timestamp) {
+  const date = new Date(timestamp * 1000);
+  const hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+  const minutes =
+    date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+  return `${hours}:${minutes}`;
+}
+
+// Function to get the day name
+function getDayName(date) {
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+}
+//Variables to store API key and API url for specific location
+const API_KEY = "b99f308a84c42a7a2c701a26eba18058";
 const CURRENT_WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=${API_KEY}`;
 const FORECAST_API_URL = `https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=${API_KEY}`;
 
-// Function to format a timestamp to a readable time format
-function formatTime(timestamp) {
-    const date = new Date(timestamp * 1000);
-    const hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
-    const minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
-    return `${hours}:${minutes}`;
-}
+const weatherApp = document.querySelector(".weather-app");
 
-const weatherApp = document.getElementById('weatherApp');
+// Get the current time
+const currentDateTime = new Date();
+const currentHours = currentDateTime.getHours();
+const currentMinutes = currentDateTime.getMinutes();
+const currentTime = `${currentHours < 10 ? "0" + currentHours : currentHours}:${
+  currentMinutes < 10 ? "0" + currentMinutes : currentMinutes
+}`;
 
 // Fetch current weather data
 fetch(CURRENT_WEATHER_API_URL)
-    .then((res) => res.json())
-    .then((currentData) => {
-        // Fetch weather forecast data for that specific location
-        fetch(FORECAST_API_URL)
-            .then((res) => res.json())
-            .then((forecastData) => {
-                // Extract the forecast data for the next 5 days
-                const forecasts = forecastData.list;
+  .then((res) => res.json())
+  .then((currentData) => {
+    // Fetch weather forecast data for that specific location
+    fetch(FORECAST_API_URL)
+      .then((res) => res.json())
+      .then((forecastData) => {
+        // Extract the forecast data for the next 5 days
+        const forecasts = forecastData.list;
 
-                // Group forecast data by day
-                const dailyForecasts = {};
-                forecasts.forEach(forecast => {
-                    const date = forecast.dt_txt.split(' ')[0];
-                    if (!dailyForecasts[date]) {
-                        dailyForecasts[date] = [];
-                    }
-                    dailyForecasts[date].push(forecast);
-                });
+        // Group forecast data by day
+        const dailyForecasts = {};
+        forecasts.forEach((forecast) => {
+          const date = forecast.dt_txt.split(" ")[0];
+          if (!dailyForecasts[date]) {
+            dailyForecasts[date] = [];
+          }
+          dailyForecasts[date].push(forecast);
+        });
 
-                // Display current weather data
-                const sunrise = formatTime(currentData.sys.sunrise);
-                const sunset = formatTime(currentData.sys.sunset);
-                weatherApp.innerHTML = `
-                    <p>City: ${currentData.name}</p>
-                    <p>Temperature: ${Math.round(currentData.main.temp * 10) / 10}°C</p>
-                    <p>Type of weather: ${currentData.weather[0].description}</p>
-                    <p>Sunrise: ${sunrise}</p>
-                    <p>Sunset: ${sunset}</p>
-                `;
+        // Display current weather data
+        const sunrise = formatTime(currentData.sys.sunrise);
+        const sunset = formatTime(currentData.sys.sunset);
+        const description = currentData.weather[0].description;
+        const temperature = Math.round(currentData.main.temp * 10) / 10;
 
-                // Display weather forecast
-                weatherApp.innerHTML += '<h2>5-Day Weather Forecast:</h2>';
-                for (const date in dailyForecasts) {
-                    const maxTemp = Math.max(...dailyForecasts[date].map(forecast => forecast.main.temp_max));
-                    const minTemp = Math.min(...dailyForecasts[date].map(forecast => forecast.main.temp_min));
-                    const dayDescription = dailyForecasts[date][0].weather[0].description;
-                    const isRainy = dailyForecasts[date].some(forecast => forecast.weather[0].main.toLowerCase().includes('rain'));
+        document.querySelector(
+          ".current-temperature"
+        ).textContent = `${temperature} °C`;
+        document.querySelector(".city").textContent = currentData.name;
+        document.querySelector(".time").textContent = `Time: ${currentTime}`;
+        document.querySelector(".description").textContent = `${description} `;
+        document.querySelector(
+          ".wind-speed"
+        ).textContent = `Wind Speed ${currentData.wind.speed} m/s`;
+        document.querySelector(
+          ".sunrise-sunset"
+        ).textContent = `Sunrise ${sunrise}   Sunset ${sunset}`;
 
-                    weatherApp.innerHTML += `
-                        <p>Date: ${date}</p>
-                        <p>Min Temperature: ${Math.round(minTemp * 10) / 10}°C</p>
-                        <p>Max Temperature: ${Math.round(maxTemp * 10) / 10}°C</p>
-                        <p>Type of weather: ${dayDescription}</p>
-                        <p>Rainy: ${isRainy ? 'Yes' : 'No'}</p>
+        // Display weather forecast
+        const forecastList = document.querySelector(".forecast-list");
+        forecastList.innerHTML = "";
+        const today = new Date(); // Get the current date
+        for (const date in dailyForecasts) {
+          const maxTemp = Math.max(
+            ...dailyForecasts[date].map((forecast) => forecast.main.temp_max)
+          );
+          const minTemp = Math.min(
+            ...dailyForecasts[date].map((forecast) => forecast.main.temp_min)
+          );
+          const dayDescription = dailyForecasts[date][0].weather[0].description;
+          const isRainy = dailyForecasts[date].some((forecast) =>
+            forecast.weather[0].main.toLowerCase().includes("rain")
+          );
+
+          // Calculate the day name for the forecast
+          const forecastDate = new Date(date);
+          let dayName = getDayName(forecastDate);
+          if (forecastDate.toDateString() === today.toDateString()) {
+            dayName = "Today";
+          }
+
+          // Extract wind speed for the day's forecast
+          const windSpeed = dailyForecasts[date][0].wind.speed;
+
+          forecastList.innerHTML += `
+                        <li>
+                            ${dayName} <img src="design/design1/assets/Group16.png" alt="Weather Icon" />
+                            ${Math.round(maxTemp * 10) / 10}°C - ${
+            Math.round(minTemp * 10) / 10
+          }°C
+                            ${windSpeed} m/s
+                        </li>
                     `;
-                }
-            })
-            .catch((err) => console.log("Error fetching forecast data: ", err));
-    })
-    .catch((err) => console.log("Error fetching current weather data: ", err));
+        }
+      })
+      .catch((err) => console.log("Error fetching forecast data: ", err));
+  })
+  .catch((err) => console.log("Error fetching current weather data: ", err));

98a8d97e3b46d77c40d5ff374004f682ac08e3e1
intehon <intehon@gmail.com>
2023-10-13 13:45:33 +0200

    Added variables for sunrise/sunset time and converted to hours and minutes

--- script.js
+++ script.js
@@ -7,10 +7,17 @@ const weatherApp = document.getElementById('weatherApp')
 fetch(API_URL)
     .then((res) => res.json())
     .then((data) => {
+
+        //Store sunrise and sunset time in variables
+        const sunrise = new Date(data.sys.sunrise * 1000)
+        const sunset = new Date(data.sys.sunset * 1000)
+
         weatherApp.innerHTML += `
         <p>City: ${data.name}</p>
         <p>Temperature: ${Math.round(data.main.temp * 10) / 10}</p>
         <p>Type of weather: ${data.weather[0].description}</p>
+        <p>Sunrise: ${sunrise.getHours() < 10 ? '0' + sunrise.getHours() : sunrise.getHours()}:${sunrise.getMinutes()}</p>
+        <p>Sunset: ${sunset.getHours() < 10 ? '0' + sunset.getHours() : sunset.getHours()}:${sunset.getMinutes()}</p>
         `
         console.log(data)
     })  
